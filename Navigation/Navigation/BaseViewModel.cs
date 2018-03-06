666fd52e8d22ca92ae76c629317e4c888b8fd30d
Bartosz Zarczynski <bartosz.zarczynski@jitsolutions.pl>
2018-03-06 15:17:14 +0100

    BaseViewModel added

---
+++ Navigation/Navigation/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Navigation
+{
+    public class BaseViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
