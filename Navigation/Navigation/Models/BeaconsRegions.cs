d8c25c15e1fe0661b856eb7938c2fb2c0b5a9cf2
Marek Przybolewski <mprzybolewski@gmail.com>
2018-03-14 14:38:27 +0100

    change MainPage layout

--- Navigation/Navigation/Models/BeaconsRegions.cs
+++ Navigation/Navigation/Models/BeaconsRegions.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 namespace Navigation.Models
 {
-    public class BeaconsRegions
+    public static class BeaconsRegions
     {
-        public BeaconRegionCoordinatesOnMap Sala1Beacon = new BeaconRegionCoordinatesOnMap("Sala1",0.095, 1.07, 0.2, 0.2);
-        public BeaconRegionCoordinatesOnMap Sala2Beacon = new BeaconRegionCoordinatesOnMap("Sala2", 0.85, 0.05, 0.2, 0.2);
+        public static BeaconRegionCoordinatesOnMap Sala1Beacon = new BeaconRegionCoordinatesOnMap("Sala1", 0.92, 0.05, 0.2, 0.2);
+        public static BeaconRegionCoordinatesOnMap Sala2Beacon = new BeaconRegionCoordinatesOnMap("Sala2", 0.08, 1.07, 0.2, 0.2);
     }
 }
