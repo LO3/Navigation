657b63f195ddb472a38ae6b4e37dfa3c76b485fd
Marek Przybolewski <mprzybolewski@gmail.com>
2018-03-14 13:06:46 +0100

    change name of one region

--- Navigation/Navigation/Models/BeaconsRegions.cs
+++ Navigation/Navigation/Models/BeaconsRegions.cs
@@ -4,6 +4,6 @@ namespace Navigation.Models
     public class BeaconsRegions
     {
         public BeaconRegionCoordinatesOnMap Sala1Beacon = new BeaconRegionCoordinatesOnMap("Sala1",0.095, 1.07, 0.2, 0.2);
-        public BeaconRegionCoordinatesOnMap Sala2Beacon = new BeaconRegionCoordinatesOnMap("Sala1", 0.85, 0.05, 0.2, 0.2);
+        public BeaconRegionCoordinatesOnMap Sala2Beacon = new BeaconRegionCoordinatesOnMap("Sala2", 0.85, 0.05, 0.2, 0.2);
     }
 }
