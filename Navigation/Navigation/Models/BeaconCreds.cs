d4bfc85f1d046f5be1aaf22764496f9bb589dc10
Marek Przybolewski <mprzybolewski@gmail.com>
2018-03-14 12:48:29 +0100

    add missing using

--- Navigation/Navigation/Models/BeaconCreds.cs
+++ Navigation/Navigation/Models/BeaconCreds.cs
@@ -3,10 +3,7 @@ namespace Navigation.Models
 {
     public class BeaconCreds
     {
-        public BeaconCreds()
-        {
-            public static readonly string UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
-            public static readonly string BEACON_ID = "test";
-        }
+        public static readonly string UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+        public static readonly string BEACON_ID = "test";
     }
 }
--- Navigation/Navigation/Models/Beacons.cs
+++ Navigation/Navigation/Models/Beacons.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Navigation.Models
 {
     public static class Beacons
