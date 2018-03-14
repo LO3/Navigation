f9e5dc6569724f6a9fe093e4a88e75cbe0e27038
Marek Przybolewski <mprzybolewski@gmail.com>
2018-03-14 13:04:04 +0100

    add models for beacons regions

---
+++ Navigation/Navigation/Models/BeaconsRegions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Navigation.Models
+{
+    public class BeaconsRegions
+    {
+        public BeaconRegionCoordinatesOnMap Sala1Beacon = new BeaconRegionCoordinatesOnMap("Sala1",0.095, 1.07, 0.2, 0.2);
+        public BeaconRegionCoordinatesOnMap Sala2Beacon = new BeaconRegionCoordinatesOnMap("Sala1", 0.85, 0.05, 0.2, 0.2);
+    }
+}
