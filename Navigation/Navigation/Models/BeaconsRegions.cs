350f2f8e27d751062612eddfcb403494a5de9945
Marek Przybolewski <mprzybolewski@gmail.com>
2018-03-15 12:55:07 +0100

    change BeaconsRegions to Rectangle

--- Navigation/Navigation/Models/BeaconsRegions.cs
+++ Navigation/Navigation/Models/BeaconsRegions.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Xamarin.Forms;
+
 namespace Navigation.Models
 {
     public static class BeaconsRegions
     {
-        public static BeaconRegionCoordinatesOnMap Sala1Beacon = new BeaconRegionCoordinatesOnMap("Sala1", 0.92, 0.05, 0.2, 0.2);
-        public static BeaconRegionCoordinatesOnMap Sala2Beacon = new BeaconRegionCoordinatesOnMap("Sala2", 0.08, 1.07, 0.2, 0.2);
+        public static Rectangle Sala1Beacon = new Rectangle(0.92, 0.05, 0.2, 0.2);
+        public static Rectangle Sala2Beacon = new Rectangle(0.08, 1.07, 0.2, 0.2);
     }
 }
