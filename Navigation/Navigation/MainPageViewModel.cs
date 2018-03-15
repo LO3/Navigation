60c99d96aba3db490136a2555631b022abdcb92a
Marek Przybolewski <mprzybolewski@gmail.com>
2018-03-15 13:24:56 +0100

    merge from implementBeacons

--- Navigation/Navigation/MainPageViewModel.cs
+++ Navigation/Navigation/MainPageViewModel.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Input;
-using IBeaconLIc.Events;
-using IBeaconLIc.Interfaces;
-using Navigation.Models;
 using Xamarin.Forms;
-using static IBeaconLIc.Events.IBeaconEvent;
 
 namespace Navigation
 {
     class MainPageViewModel : BaseViewModel
     {
         private string _classroom;
-        public string EntryClassroom
-        {
+        public string EntryClassroom {
             get
             {
                 return _classroom;
@@ -51,7 +46,7 @@ namespace Navigation
             {
                 return _isListVisible;
             }
-            set
+            set 
             {
                 _isListVisible = value;
                 RaisePropertyChanged();
@@ -70,18 +65,10 @@ namespace Navigation
             _isListVisible = false;
             DismissListCommand = new Command(DismissList);
 
-            MessagingCenter.Subscribe<MainPage, bool>(this, "Entry", (sender, arg) =>
-            {
+            MessagingCenter.Subscribe<MainPage, bool>(this, "Entry", (sender, arg) => {
                 IsListVisible = arg;
                 FilteredClassroomList = _classroomList;
-            });
-
-
-
-            var iBeaconService = DependencyService.Get<IiBeaconService>();
-            iBeaconService.OnBeaconDataChanged += new IBeaconHandler(Notified);
-            iBeaconService.Initialize();
-
+            });  
         }
 
         private static List<string> _classroomList = new List<string>
@@ -93,54 +80,5 @@ namespace Navigation
             "Sekretariat",
             "Kantor woźnego"
         };
-
-        private Rectangle _pikachuPosition;
-        public Rectangle PikachuPosition
-        {
-            get
-            {
-                return _pikachuPosition;
-            }
-
-            set
-            {
-                _pikachuPosition = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        bool _isPikachuVisible = false;
-        public bool IsPikachuVisible
-        {
-            get
-            {
-                return _isPikachuVisible;
-            }
-            set
-            {
-                _isPikachuVisible = value;
-                RaisePropertyChanged();
-            }
-        }
-
-
-        public void Notified(object source, IBeaconEvent e)
-        {
-            var beacon = Beacons.BeaconList.Where(b => b.Major == e.Major && b.Minor == e.Minor).FirstOrDefault();
-
-            if (beacon != null)
-            {  
-                if(e.distanceDescription == "You did it")
-                {
-                    PikachuPosition = beacon.Region;
-                    IsPikachuVisible = true;
-                }
-                else
-                {
-                    IsPikachuVisible = false;
-                }
-               
-            }
-        }
     }
 }
--- Navigation/Navigation/Models/BeaconCreds.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Navigation.Models
-{
-    public class BeaconCreds
-    {
-        public static readonly string UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
-        public static readonly string BEACON_ID = "test";
-    }
-}
